cb45b80ca7ba2f602dc0e9dbeef24450fe8fedaa
EugeneL11 <eugenelee0605@gmail.com>
2023-10-06 01:25:56 -0600

    initial commit, starter file with just the basic imports

---
+++ proxy.cpp
@@ -0,0 +1,13 @@
+// CPSC 441 Assignment 2
+// Eugene Lee - 30137489
+// Frog Web Proxy
+
+// Necessary imports
+#include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+
+using namespace std;
